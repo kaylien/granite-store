66e648f16380ae77db249352559ac96c9a652317
Kaylien Phan <kaylienphan@gmail.com>
2019-10-09 14:09:47 -0700

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+class NavBar extends React.Component{
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+        );
+    }
+}
+
+export default NavBar
